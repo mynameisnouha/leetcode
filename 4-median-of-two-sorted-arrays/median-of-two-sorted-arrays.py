22687fa4a733ab7e7da30bc3169ab55b2c7405ac
mynameisnouha <73719011+mynameisnouha@users.noreply.github.com>
2025-02-26 16:19:22 +0100

    Time: 5 ms (43.19%) | Memory: 12.7 MB (10.96%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -9,7 +9,7 @@ class Solution(object):
         sorted_arr=sorted(nums1+nums2)
         n=len(sorted_arr)
         if n % 2 == 1:
-            return float(sorted_arr[n // 2])
+            return sorted_arr[n // 2]
         else:
             
             return ((float(sorted_arr[n // 2 - 1]) + float(sorted_arr[n // 2])) / 2)
